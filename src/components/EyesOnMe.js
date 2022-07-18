d1f781c2c2c3b51cf0c794a0b42422c74de105f6
Otieno-derrick <otieno.derrick@student.moringaschool.com>
2022-07-18 22:43:53 +0300

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React from 'react'
+
+const EyesOnMe = () => {
+
+    function manageFocus() {
+        console.log('Good!');
+    }
+    function manageBlur (){
+        console.log('Hey! Eyes on me!');
+    }
+  return (
+        <button onFocus={manageFocus} onBlur={manageBlur}>Eyes on me</button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from 'react';
+
+const Keypad = () => {
+
+    function manageChange() {
+        console.log("Entering password...");
+
+    }
+  return (
+     <input type="password" onChange={manageChange} />
+  );
+}
+
+export default Keypad;
